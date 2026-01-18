f0400c903e60aa65e6d5d2076c5ebac467da9629
c1570 <c1570@users.noreply.github.com>
2026-01-18 02:43:56 +0100

    patch KERNAL to skip RAM check

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 if [ ! -e c64-roms.h ]; then
   wget https://raw.githubusercontent.com/floooh/chips-test/refs/heads/master/examples/roms/c64-roms.h
+  # patch: skip RAM check
+  perl -i -pe 's/0xb3, 0xa8, 0xa9, 0x03, 0x85, 0xc2, 0xe6, 0xc2, 0xb1, 0xc1,/0xb3, 0xa8, 0xa9, 0x9f, 0x85, 0xc2, 0xe6, 0xc2, 0xb1, 0xc1,/gm' c64-roms.h
 fi
 if [ ! -e c1541-roms.h ]; then
   wget https://raw.githubusercontent.com/floooh/chips-test/refs/heads/master/examples/roms/c1541-roms.h
