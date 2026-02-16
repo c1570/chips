214f72fed1f858c07db8a392963e5fe2dcebbf22
c1570 <c1570@users.noreply.github.com>
2026-02-16 21:07:51 +0100

    RP2: fix cycle_tracing alignment

--- rp2040/cycle_tracing.h
+++ rp2040/cycle_tracing.h
@@ -1,25 +1,27 @@
 #ifndef __riscv
-#define PROF_TP(tag,tp,par) \
+#define PROF_TP(par) \
   __asm__ __volatile__ ( \
     ".syntax unified\n\t" \
     "b 1f\n\t" \
     ".word 0xffffabcd\n\t" \
-    ".asciz "#tag"\n\t" \
+    ".asciz "#par"\n\t" \
+    ".balign 4\n\t" \
     "1:\n\t" \
   );
 #else
-#define PROF_TP(tag,tp,par) \
+#define PROF_TP(par) \
   __asm__ __volatile__ ( \
     "j 1f\n\t" \
     ".word 0xffffabcd\n\t" \
-    ".asciz "#tag"\n\t" \
+    ".asciz "#par"\n\t" \
+    ".balign 4\n\t" \
     "1:\n\t" \
   );
 #endif
 
-#define cycle_info(a) PROF_TP(a, "0x54505052", "0x00000000")
+#define cycle_info(a) PROF_TP(a)
 #ifdef CYCLE_TRACE
-#define cycle_trace(a) PROF_TP(a, "0x54505052", "0x00000000")
+#define cycle_trace(a) PROF_TP(a)
 #else
 #define cycle_trace(a)
 #endif
